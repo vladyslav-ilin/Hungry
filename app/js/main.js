b4965634ff64f3d31b65e65e43c23ee5f64921db
vladyslav-ilin <vlad110696@gmail.com>
2021-02-13 14:50:24 +0200

    event

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 (function($){
 	$(document).ready(function() {
 		// Code
@@ -8,4 +9,30 @@
 			$('.menu__wrapper').toggleClass('menu__wrapper--open');
 		});
 	});
+
+   $('.tabs__btn').on('click', function() {
+      $('.tabs__all').toggleClass('tabs__all--active');
+   })
+
+   $('.tabs__list').click(function (operation) {
+      operation.preventDefault();
+      $('.tabs__list').removeClass('tabs__list--active');
+      $('.tabs__wrapper--active').removeClass('tabs__wrapper--active');
+
+      $(this).addClass('tabs__list--active');
+      let item = $(this).find('a').attr('href');
+      $(item).addClass('tabs__wrapper--active');
+   })
 })(jQuery);
+
+new Swiper('.swiper__container', {
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+	// effect: 'fade',
+	// fadeEffect: {
+	// 	crossFade: true
+	// }
+});
